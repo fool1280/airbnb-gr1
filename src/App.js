2817ec23797e892e2cfb53a27381a3f784027657
Ilona Pak <inkha.pak@gmail.com>
2020-07-03 23:25:33 +0700

    add experience detail page and edit create experience form

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./App.css";
 import ExperiencesList from "./components/ExperiencesList";
 import AddExperience from "./components/AddExperience";
+import ExperienceDetails from "./components/ExperienceDetails";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/create">
-                    <AddExperience />
-                </Route>
-                <Route path="/">
-                    <ExperiencesList />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create">
+          <AddExperience />
+        </Route>
+        <Route path="/:id">
+          <ExperienceDetails />
+        </Route>
+        <Route path="/">
+          <ExperiencesList />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
